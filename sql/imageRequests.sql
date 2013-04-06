75646513db99fd575ea6a01ad87d6ad0e64c8e28
Michael Robinson <mike@pagesofinterest.net>
2013-04-07 02:14:24 +1300

    Added SQL

---
+++ sql/imageRequests.sql
@@ -0,0 +1,8 @@
+delimiter $$
+
+CREATE TABLE `imageRequests` (
+  `image` varchar(100) NOT NULL,
+  `type` varchar(45) NOT NULL,
+  PRIMARY KEY (`type`,`image`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
