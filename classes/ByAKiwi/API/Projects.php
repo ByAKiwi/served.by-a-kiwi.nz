8ca1595add7d9a33734777c84e58b016c483d4fb
Michael Robinson <mike@pagesofinterest.net>
2013-04-18 04:54:31 -0600

    update headers

--- classes/ByAKiwi/API/Projects.php
+++ classes/ByAKiwi/API/Projects.php
@@ -11,12 +11,14 @@ class Projects extends Tonic\Resource {
   
   /**
    * @method GET
+   * 
    */
   public function coded() {
     $requests = ImageRequest::findAllCoded($this->container['database']);
 
     return new Response(200, json_encode(array('projects' => $requests)), array(
-      'Content-Type' => 'application/json'
+      'AccessControlAllowOrigin' => '*',
+      'ContentType' => 'application/json'
     ));
   }
 }
